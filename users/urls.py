1b6b377adfb41f8f21afa957189f990a4662aae0
carl-claw <carl.claw.me@gmail.com>
2026-03-02 16:31:24 +0100

    Added group statistics feature.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import register, group_statistics
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('group/<int:group_id>/statistics/', group_statistics, name='group_statistics'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, get_object_or_404
+from .models import Group
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+def group_statistics(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    total_steps = sum(user.steps for user in group.members.all())
+    average_steps = total_steps / group.members.count() if group.members.count() > 0 else 0
+
+    context = {
+        'group': group,
+        'total_steps': total_steps,
+        'average_steps': average_steps,
+        'member_count': group.members.count(),
+    }
+    return render(request, 'group_statistics.html', context)
